86672a08036bc3724b31bdc399bfd05531f63bb2
julian vega <juliandresvega0323@gmail.com>
2018-04-14 18:38:55 -0500

    commit inicial

---
+++ javaScript.js
@@ -0,0 +1,23 @@
+
+  $("#registro-clientes").click(function(event) {
+    $("#cargar-pagina").load('RegistroClientes.html');
+  });
+
+
+  $("#contacto-cargar").click(function(event) {
+    $("#lugar-cargar").load('Contacto.html');
+  });
+
+
+  $("#crear-cuenta").click(function(event) {
+    $("#cargar-crearCuenta").load('CrearCuenta.html');
+  });
+
+  $("#registroMensajeros").click(function(event) {
+    $("#cargar-pagina").load('RegistrarMensajeros.html');
+  });
+
+
+  $("#registroEnvios").click(function(event) {
+    $("#cargar-pagina").load('RegistrarEnvios.html');
+  });
